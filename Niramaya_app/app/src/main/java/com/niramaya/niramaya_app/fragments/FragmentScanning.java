1da736f2b11f5b09864217455c70403a30d7a0b7
Bharath <132049800+pujariBharath@users.noreply.github.com>
2023-09-13 21:59:54 +0530

    changes

---
+++ Niramaya_app/app/src/main/java/com/niramaya/niramaya_app/fragments/FragmentScanning.java
@@ -0,0 +1,51 @@
+package com.niramaya.niramaya_app.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.niramaya.niramaya_app.R;
+public class FragmentScanning extends AppCompatActivity
+{
+    Button btn_scan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_scan =findViewById(R.id.btn_scan);
+        btn_scan.setOnClickListener(v->
+        {
+            scanCode();
+        });
+    }
+
+    private void scanCode()
+    {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLaucher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLaucher = registerForActivityResult(new ScanContract(), result->
+    {
+        if(result.getContents() !=null)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i)
+                {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+    });
+
+}
