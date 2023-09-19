9f632db93972e6406e649a5d5e42e29d90aed3af
Nilesh Yerunkar <nyerunkar@gmail.com>
2023-09-19 16:46:56 +0530

    QR Scanner

---
+++ Niramaya_app/app/src/main/java/com/niramaya/niramaya_app/activities/Activity_CapturePortrait.java
@@ -0,0 +1,7 @@
+package com.niramaya.niramaya_app.activities;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class Activity_CapturePortrait extends CaptureActivity
+{
+}
--- Niramaya_app/app/src/main/java/com/niramaya/niramaya_app/fragments/FragmentScanning.java
+++ Niramaya_app/app/src/main/java/com/niramaya/niramaya_app/fragments/FragmentScanning.java
@@ -1,51 +1,101 @@
 package com.niramaya.niramaya_app.fragments;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultLauncher;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+import com.niramaya.niramaya_app.Constants;
 import com.niramaya.niramaya_app.R;
-public class FragmentScanning extends AppCompatActivity
-{
-    Button btn_scan;
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btn_scan =findViewById(R.id.btn_scan);
-        btn_scan.setOnClickListener(v->
-        {
-            scanCode();
-        });
-    }
+import com.niramaya.niramaya_app.activities.Activity_CapturePortrait;
+import com.niramaya.niramaya_app.customview.Button_custom;
+import com.niramaya.niramaya_app.customview.TextView_custom;
 
+public class FragmentScanning extends BaseFragment
+{
+    private SharedPreferences sharedPref = null;
+    private Button_custom btScan;
+    private TextView_custom tvResult;
     private void scanCode()
     {
-        ScanOptions options = new ScanOptions();
-        options.setPrompt("Volume up to flash on");
-        options.setBeepEnabled(true);
-        options.setOrientationLocked(true);
-        options.setCaptureActivity(CaptureAct.class);
-        barLaucher.launch(options);
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.fragment_scanning, container, false);
+        sharedPref = getActivity().getSharedPreferences(Constants.PACKAGE_NAME, Context.MODE_PRIVATE);
+
+        tvResult = (TextView_custom) rootView.findViewById(R.id.tvResult);
+
+        btScan = (Button_custom) rootView.findViewById(R.id.btScan);
+        btScan.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startBarcodeScan();
+            }
+        });
+
+
+        return rootView;
     }
 
-    ActivityResultLauncher<ScanOptions> barLaucher = registerForActivityResult(new ScanContract(), result->
+    private void startBarcodeScan()
     {
-        if(result.getContents() !=null)
-        {
-            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-            builder.setTitle("Result");
-            builder.setMessage(result.getContents());
-            builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
-            {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i)
-                {
-                    dialogInterface.dismiss();
-                }
-            }).show();
-        }
-    });
 
+        Dexter.withContext(getActivity())
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener()
+                {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response)
+                    {
+                        ScanOptions options = new ScanOptions();
+                        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE);
+                        options.setBeepEnabled(false);
+                        options.setBarcodeImageEnabled(true);
+                        options.setOrientationLocked(true);
+                        options.setCaptureActivity(Activity_CapturePortrait.class);
+                        barcodeLauncher.launch(options);
+                    }
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response)
+                    {
+
+                    }
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token)
+                    {
+
+                    }
+                }).check();
+    }
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
+            result -> {
+                if(result.getContents() == null)
+                {
+                }
+                else
+                {
+                    String sLastScanned = result.getContents();
+                    String sContents = ""+sLastScanned.replaceAll("\n","");
+                    tvResult.setText(""+sContents);
+                }
+            });
 }
